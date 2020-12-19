9740b4e12fb62aa128d93c878c5b8dbd620fa97e
Raul Rodriguez <raulrobi@gmail.com>
2020-12-19 20:24:51 -0300

    Class 117 - useMemo

--- src/components/06-memos/MemoHook.js
+++ src/components/06-memos/MemoHook.js
@@ -1,23 +1,26 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import {useCounterQuote} from "../../hooks/useCounterQuote";
 import "./styles.css";
+import {hardProcess} from "../../helpers/hardProcess";
 
 export const MemoHook = () => {
-    const {counter, increment} = useCounterQuote(10);
+    const {counter, increment} = useCounterQuote(5000);
     const [show, setShow] = useState(true);
 
-    const hardProcess = (iteractions) => {
+    // Si el counter cambia (segundo parámetro) necesito una nueva versión memorizada
+    // de la función hardProcess
+    //Solo se dispara la función cuando el counter cambia. Si presiono el botón
+    // Show/Hide, no se ejecuta
+    const memoHardProcess = useMemo(() => hardProcess(counter), [counter]);
 
-for (let i = 0; i < iteractions; i++) {
-    console.log()
-}
-    
     return (
         <div>
             <h1>Memo Hook</h1>
             <hr></hr>
 
             <p>Counter: {counter}</p>
+
+            <p>{memoHardProcess}</p>
 
             <button className="btn btn-primary mr-3" onClick={increment}>
                 +1
---
+++ src/helpers/hardProcess.js
@@ -0,0 +1,6 @@
+export const hardProcess = (iteractions) => {
+    for (let i = 0; i < iteractions; i++) {
+        console.log("Here we go!");
+    }
+    return `${iteractions} iteractions done`;
+};
