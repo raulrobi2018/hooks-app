7c5a95584f32476bfb6e1e2c22bd1ea3372854cb
Raul Rodriguez <raulrobi@gmail.com>
2021-01-06 15:44:55 -0300

    Class 139 - Link and NavLink

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -1,17 +1,29 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
 import {AboutScreen} from "./AboutScreen";
 import {HomeScreen} from "./HomeScreen";
 import {LoginScreen} from "./LoginScreen";
+import {NavBar} from "./NavBar";
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
+                <NavBar />
                 <Switch>
                     <Route exact path="/" component={HomeScreen} />
                     <Route exact path="/about" component={AboutScreen} />
                     <Route exact path="/login" component={LoginScreen} />
+
+                    {/* Si ingresan una ruta que no existe, entra aquí y redirecciona a / (barra).
+                     También se puede crear una página 404 y poner otro Route al final que direccione
+                     a esa página */}
+                    <Redirect to="/" />
                 </Switch>
             </div>
         </Router>
