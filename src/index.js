c17bd05fac28637e4b18aa0aa6e1bde804cf02df
Raul Rodriguez <raulrobi@gmail.com>
2021-01-06 08:49:11 -0300

    Class 138 - Configuring Router in React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {TodoApp} from "./components/08-useReducer/TodoApp";
+import {MainApp} from "./components/09-useContext/MainApp";
+// import {TodoApp} from "./components/08-useReducer/TodoApp";
 // import {Padre} from "./components/07-tarea-memo/Padre";
 // import {CallbackHook} from "./components/06-memos/CallbackHook";
 // import {MemoHook} from "./components/06-memos/MemoHook";
@@ -16,6 +17,7 @@ import {TodoApp} from "./components/08-useReducer/TodoApp";
 
 // // import {HooksApp} from "./HooksApp";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+// ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
 
 // import "./components/08-useReducer/intro-reducer";
