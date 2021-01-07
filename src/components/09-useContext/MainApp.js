d8585d43b1bfbf1cc7b0eef5252bf5a9afca9beb
Raul Rodriguez <raulrobi@gmail.com>
2021-01-07 09:20:41 -0300

    Class 140 - CreateContext and useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,6 +1,18 @@
 import React from "react";
 import {AppRouter} from "./AppRouter";
+import {UserContext} from "./UserContext";
 
 export const MainApp = () => {
-    return <AppRouter />;
+    const user = {
+        id: 1,
+        name: "Raul",
+        age: 42,
+        email: "raulrobi@gmail.com"
+    };
+
+    return (
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
+    );
 };
