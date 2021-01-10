22fd5999d7d598f5e1813ff9c8a3c1704c4639c8
Raul Rodriguez <raulrobi@gmail.com>
2021-01-09 21:28:06 -0300

    Class 147 - Testing functions in useCounter - CustomHook

--- src/tests/useCounter.test.js
+++ src/tests/useCounter.test.js
@@ -1,4 +1,4 @@
-import {renderHook} from "@testing-library/react-hooks";
+import {renderHook, act} from "@testing-library/react-hooks";
 import {useCounter} from "../hooks/useCounter";
 
 describe("Testing useCounter", () => {
@@ -11,5 +11,48 @@ describe("Testing useCounter", () => {
         expect(typeof result.current.decrement).toBe("function");
         expect(typeof result.current.increment).toBe("function");
         expect(typeof result.current.reset).toBe("function");
+    });
+
+    test("should return the state with a value of 100", () => {
+        const {result} = renderHook(() => useCounter(100));
+
+        expect(result.current.state).toBe(100);
+    });
+
+    test("should increment the state in 1", () => {
+        const {result} = renderHook(() => useCounter(100));
+        const {increment} = result.current;
+
+        act(() => {
+            increment();
+        });
+
+        const {state} = result.current;
+        expect(state).toBe(101);
+    });
+
+    test("should decrement the state in 1", () => {
+        const {result} = renderHook(() => useCounter(100));
+        const {decrement} = result.current;
+
+        act(() => {
+            decrement();
+        });
+
+        const {state} = result.current;
+        expect(state).toBe(99);
+    });
+
+    test("should reset the state", () => {
+        const {result} = renderHook(() => useCounter(100));
+        const {increment, reset} = result.current;
+
+        act(() => {
+            increment();
+            reset();
+        });
+
+        const {state} = result.current;
+        expect(state).toBe(100);
     });
 });
