e28e65fdeaff725f7c04d7756cd2e4bb5529654c
Raul Rodriguez <raulrobi@gmail.com>
2021-01-10 20:45:13 -0300

    Class 148 - Testing useForm - CustomHook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,38 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 export const useForm = (initialState = {}) => {
-    const [values, setvalues] = useState(initialState);
+  const [values, setvalues] = useState(initialState);
 
-    const reset = () => {
-        setvalues(initialState);
-    };
+  const reset = () => {
+    setvalues(initialState);
+  };
 
-    // Desestructuro target del event
-    const handleInputChange = ({target}) => {
-        setvalues({
-            ...values,
-            // Le pone el nombre del input recibido (atributo name) y le asigna el valor cargado
-            [target.name]: target.value
-        });
-    };
+  // Desestructuro target del event
+  const handleInputChange = ({ target }) => {
+    setvalues({
+      ...values,
+      // Le pone el nombre del input recibido (atributo name) y le asigna el valor cargado
+      [target.name]: target.value
+    });
+  };
 
-    const {email} = values;
+  const { email } = values;
 
-    useEffect(() => {
-        console.log("Cambió el email");
-    }, [email]);
+  useEffect(() => {
+    console.log("Cambió el email");
+  }, [email]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(values);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
 
-    const functions = {
-        handleInputChange,
-        handleSubmit,
-        reset
-    };
+  const functions = {
+    handleInputChange,
+    handleSubmit,
+    reset
+  };
 
-    // Retorna el estado y las funciones para manejarlas en otros componentes
-    return [values, functions];
+  // Retorna el estado y las funciones para manejarlas en otros componentes
+  return [values, functions];
 };
--- src/tests/useForm.test.js
+++ src/tests/useForm.test.js
@@ -1,8 +1,34 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useForm } from "../hooks/useForm";
+
+
 describe("Testing useForm.js", () => {
     const initialForm = {
         name: "Raul",
         email: "raulrobi@gmail.com"
     };
 
-    test("should ", () => {});
+    test("should return a the default form", () => {
+        const { result } = renderHook(() => useForm(initialForm));
+        // Extraigo valores y funciones retornadas por el useForm
+        const [values, funcs] = result.current;
+        const { handleInputChange, reset } = funcs;
+
+        expect(values).toEqual(initialForm);
+        expect(typeof handleInputChange).toBe("function");
+        expect(typeof reset).toBe("function");
+    });
+
+    test("should change the name value of the form", () => {
+        const { result } = renderHook(() => useForm(initialForm));
+        // Extraigo valores y funciones retornadas por el useForm
+        const [values, funcs] = result.current;
+        const { handleInputChange } = funcs;
+    });
+
+    test("should reset the form", () => {
+        const { result } = renderHook(() => useForm());
+
+        // expect(result.current.state).toBe(10); expect(typeof result.current.decrement).toBe("function"); expect(typeof result.current.increment).toBe("function"); expect(typeof result.current.reset).toBe("function");
+    });
 });
