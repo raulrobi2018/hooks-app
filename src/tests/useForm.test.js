2660d741fc29c633674cb08295f90f09c62dfcf3
Raul Rodriguez <raulrobi@gmail.com>
2021-01-11 09:44:33 -0300

    Class 148 - Testing useForm - CustomHook

--- src/tests/useForm.test.js
+++ src/tests/useForm.test.js
@@ -1,6 +1,5 @@
-import { renderHook, act } from "@testing-library/react-hooks";
-import { useForm } from "../hooks/useForm";
-
+import {renderHook, act} from "@testing-library/react-hooks";
+import {useForm} from "../hooks/useForm";
 
 describe("Testing useForm.js", () => {
     const initialForm = {
@@ -9,10 +8,10 @@ describe("Testing useForm.js", () => {
     };
 
     test("should return a the default form", () => {
-        const { result } = renderHook(() => useForm(initialForm));
+        const {result} = renderHook(() => useForm(initialForm));
         // Extraigo valores y funciones retornadas por el useForm
         const [values, funcs] = result.current;
-        const { handleInputChange, reset } = funcs;
+        const {handleInputChange, reset} = funcs;
 
         expect(values).toEqual(initialForm);
         expect(typeof handleInputChange).toBe("function");
@@ -20,15 +19,47 @@ describe("Testing useForm.js", () => {
     });
 
     test("should change the name value of the form", () => {
-        const { result } = renderHook(() => useForm(initialForm));
+        const {result} = renderHook(() => useForm(initialForm));
         // Extraigo valores y funciones retornadas por el useForm
-        const [values, funcs] = result.current;
-        const { handleInputChange } = funcs;
+        const [, funcs] = result.current;
+        const {handleInputChange} = funcs;
+
+        act(() => {
+            handleInputChange({
+                target: {
+                    name: "name",
+                    value: "Mellisa"
+                }
+            });
+        });
+
+        const [values] = result.current;
+
+        console.log(values);
+
+        expect(values).toEqual({...initialForm, name: "Mellisa"});
     });
 
     test("should reset the form", () => {
-        const { result } = renderHook(() => useForm());
+        const {result} = renderHook(() => useForm(initialForm));
+        // Extraigo valores y funciones retornadas por el useForm
+        const [, funcs] = result.current;
+        const {handleInputChange, reset} = funcs;
 
-        // expect(result.current.state).toBe(10); expect(typeof result.current.decrement).toBe("function"); expect(typeof result.current.increment).toBe("function"); expect(typeof result.current.reset).toBe("function");
+        act(() => {
+            handleInputChange({
+                target: {
+                    name: "name",
+                    value: "Mellisa"
+                }
+            });
+
+            reset();
+        });
+
+        const [values] = result.current;
+
+        // Aqui se esperaría que values sea igual al initialForm porque hacemos un reset
+        expect(values).toEqual({...initialForm});
     });
 });
