ae06dfcc486d1b99c42a0b70b88d63955065bbbb
Raul Rodriguez <raulrobi@gmail.com>
2021-01-18 08:52:02 -0300

    Class 155 - Testing TodoList

--- src/tests/components/08-useReducer/todoList.test.js
+++ src/tests/components/08-useReducer/todoList.test.js
@@ -3,18 +3,22 @@ import { TodoList } from "../../../components/08-useReducer/TodoList";
 import { demoTodos } from "../../resources/demoTodos";
 
 describe("Testing todoList component", () => {
-            const handleDel = jest.fn();
-            const handleTog = jest.fn();
-            console.log(demoTodos);
-            const wrapper = shallow( < TodoList todoList = { demoTodos }
-                handleDelete = { handleDel }
-                handleToggle = { handleTog }
+  const handleDel = jest.fn();
+  const handleTog = jest.fn();
+  const wrapper = shallow(
+    <TodoList
+      todoList={demoTodos}
+      handleDelete={handleDel}
+      handleToggle={handleTog}
+    />
+  );
 
-                />);
+  test("should display the component correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-                test("should display the component correctly", () => {
-                    expect(wrapper).toMatchSnapshot();
-                });
-
-
-            });
+  test("should have 2 todos item", () => {
+    console.log(wrapper.html);
+    expect(wrapper.find("TodoItem").length);
+  });
+});
